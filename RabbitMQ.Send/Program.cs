23e2b39cc9f1393c04da79de0b185e6ebd480164
yao <yao780913@gmail.com>
2024-06-25 17:03:39 +0800

    [feat] RabbitMQ tutorial - "Hello World!"

---
+++ RabbitMQ.Send/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "Hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "Hello world";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: "Hello",
+    basicProperties: null,
+    body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQ.Send/readme.md
@@ -0,0 +1,6 @@
+﻿# RabbitMQ tutorial
+### Introduction
+- Prerequisites  
+  `docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management`
+## Tutorial - "Hello World!"
+## Tutorial - Work Queues
