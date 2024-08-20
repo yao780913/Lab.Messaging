28a3467c764311778dc243cec62530065ea4a441
yao <yao780913@gmail.com>
2024-08-20 10:47:33 +0800

    [feat] azure service bus - create message and receive message with queue

---
+++ ServiceBus.Api/Controllers/QueueController.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceBus.Api.Controllers;
+
+[ApiController]
+public class QueueController : ControllerBase
+{
+    private readonly string? _connectionString;
+    private readonly string? _queueName;
+
+    public QueueController (IConfiguration configuration)
+    {
+        _connectionString = configuration["AzureServiceBus:ConnectionString"];
+        _queueName = configuration["AzureServiceBus:QueueName"];
+        
+        if (string.IsNullOrWhiteSpace(_connectionString))
+        {
+            throw new InvalidOperationException("ServiceBus connection string is missing");
+        }
+        
+        if (string.IsNullOrWhiteSpace(_queueName))
+        {
+            throw new InvalidOperationException("ServiceBus queue name is missing");
+        }
+    }
+    [HttpPost("Enqueue")]
+    public async Task<IActionResult> Enqueue ([FromBody] List<string> contexts)
+    {
+        await using var client = new ServiceBusClient(_connectionString);
+        await using var sender = client.CreateSender(_queueName);
+        
+        using var messageBatch = await sender.CreateMessageBatchAsync();
+        foreach (var text in contexts)
+        {
+            var message = new ServiceBusMessage(text);
+            if (messageBatch.TryAddMessage(message) is false)
+            {
+                throw new InvalidOperationException($"The message is too large to fit in the batch.");
+            }
+        }
+
+        await sender.SendMessagesAsync(messageBatch);
+
+        return Ok();
+    }
+
+    [HttpPost("Receive")]
+    public async Task<IActionResult> Receive ()
+    {
+        await using var client = new ServiceBusClient(_connectionString);
+        await using var receiver = client.CreateReceiver(_queueName);
+
+        var message = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(3));
+        if (message is null)
+        {
+            return NoContent();
+        }
+        
+        var body = message.Body.ToString();
+        await receiver.CompleteMessageAsync(message);
+        return Ok(body);
+    }
+    
+    [HttpPost("ReceiveBatch")]
+    public async Task<IActionResult> ReceiveBatch ([FromQuery]int len = 10)
+    {
+        await using var client = new ServiceBusClient(_connectionString);
+        await using var receiver = client.CreateReceiver(_queueName);
+
+        var messages = await receiver.ReceiveMessagesAsync(len, TimeSpan.FromSeconds(3));
+        if (messages.Count == 0)
+        {
+            return NoContent();
+        }
+
+        var body = new StringBuilder();
+        foreach (var message in messages)
+        {
+            body.AppendLine(message.Body.ToString());
+            await receiver.CompleteMessageAsync(message);
+        }
+
+        return Ok(body.ToString());
+    }
+}
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
-﻿# RabbitMQ tutorial
+﻿## RabbitMQ tutorial
 ### Introduction
 - Prerequisites  
   `docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management`
-## Tutorial - "Hello World!"
-## Tutorial - Work Queues
-### Round-robin dispatching
+### Tutorial - "Hello World!"
+### Tutorial - Work Queues
+#### Round-robin dispatching
 - Run 2 workers
   ```shell
   # shell 1
@@ -47,3 +47,14 @@
   # => [x] Received Fourth message....
   # => [x] Done
   ```
+  
+## Azure Service Bus
+https://igouist.github.io/post/2022/08/azure-service-bus/
+
+### Queue
+1. define queueName and connectionString
+2. `POST /queue/enqueue` to send a message to the queue
+3. `GET /queue/Receive` to receive a message from the queue
+4. `GET /queue/ReceiveBatch` to receive messages from the queue
+
+> If you do not call receiver.CompleteMessageAsync(message) in Azure Service Bus, the message will not be marked as completed. This means the message will remain in the queue and will be available for redelivery. Essentially, the message will be considered as not processed successfully, and it will be retried according to the retry policy configured for the queue. This can lead to duplicate processing of the same message.
